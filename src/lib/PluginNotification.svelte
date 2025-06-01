2910c3907ff7564a947fedd7e702079d9809499a
Gerry Wagner <gerry.wagner@wabtec.com>
2025-05-31 21:10:52 -0400

    added PluginStore and modified count to use context api

--- src/lib/PluginNotification.svelte
+++ src/lib/PluginNotification.svelte
@@ -4,15 +4,11 @@ import { isPermissionGranted, requestPermission, sendNotification } from '@tauri
 
 async function notify() {
     let permissionGranted = await isPermissionGranted();
-
     if (!permissionGranted) {
         const permission = await requestPermission();
         permissionGranted = permission === 'granted';
     }
-
-    if (permissionGranted) {
-        sendNotification({ title: 'Tauri', body: 'Tauri is awesome!' });
-    }
+    if (permissionGranted) { sendNotification({ title: 'Tauri', body: 'Tauri is awesome!' }) }
 }
 </script>
 
