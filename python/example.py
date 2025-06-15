ef74e6b7d84c637de25061897efb065d21f1bb47
Thomas-Smiley <tvs5939@psu.edu>
2025-06-15 16:16:47 -0600

    updated pipeline to run tests and save report, and added a python test example

---
+++ python/example.py
@@ -0,0 +1,43 @@
+"""
+Example Module
+
+This contains functions to add and subtract numbers.
+
+Functions:
+    add_nums(a, b): Adds the input parameters together.
+    subtract_nums(a, b): Subtracts the input parameters.
+"""
+
+
+def add_nums(a, b):
+    """Adds the two input variables together.
+
+    param a: First input variable, defaults to None
+    :type a: int
+    :param b: Second input variable, defaults to None
+    :type b: int
+
+    raises [ErrorType]: [ErrorDescription]
+
+    :return: Sum of input variables.
+    :rtype: int
+    """
+
+    return a + b
+
+
+def subtract_nums(a, b):
+    """Subtracts the two input variables together.
+
+    param a: First input variable, defaults to None
+    :type a: int
+    :param b: Second input variable, defaults to None
+    :type b: int
+
+    raises [ErrorType]: [ErrorDescription]
+
+    :return: Difference of input variables.
+    :rtype: int
+    """
+
+    return a - b
