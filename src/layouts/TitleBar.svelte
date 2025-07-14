94c872097cbd472ed65aec4ce1e6f1f9ce33754d
glwagnerii <glwagnerii@gmail.com>
2025-07-13 21:19:02 -0400

    create popover menu

--- src/layouts/TitleBar.svelte
+++ src/layouts/TitleBar.svelte
@@ -2,12 +2,22 @@
   import { Button } from '../components'
   import { openUrl } from '@tauri-apps/plugin-opener'
   import { arch, locale, platform, type, version } from '@tauri-apps/plugin-os'
-  import { message } from '@tauri-apps/plugin-dialog'
+  import { open, message } from '@tauri-apps/plugin-dialog'
   import { invoke } from '@tauri-apps/api/core'
   import { type Detection } from '../store/appSlice'
 
+  import { useDispatch } from '../store'
+  const dispatch = useDispatch()
+
   const title = ''
   const openGoogle = async () => { await openUrl('https://www.google.com') }
+
+  let popoverMenu: HTMLUListElement | null = null
+
+  function handleSelect(action: () => void) {
+    action()
+    popoverMenu?.hidePopover()
+  }
 
   const getOS = async () => {
     const info = `Platform: ${platform()}\nVersion: ${version()}\nType: ${type()}\nArch: ${arch()}\nLocale: ${await locale()}`
@@ -27,6 +37,13 @@
     await message(fullMessage, { title: 'Classific-Cam OS Information', kind: 'info' })
   }
 
+  const openFolder = async () => {
+    const file = await open({ multiple: false, directory: false })
+    await message(`You selected: ${file}`, { title: 'Classifi-Cam', kind:'info' })
+  }
+
+  const viewPath = () => dispatch({ type: 'app/viewPath' })
+
   const btnClass = 'btn-ghost border-none shadow-none transition-colors py-2 h-full px-1'
 </script>
 
@@ -38,13 +55,17 @@
     <div class='block flex-shrink truncate selected'>{title}</div>
   </div>
   <div class='flex flex-1 justify-end items-center h-full min-w-min nodrag order-2 space-x-1'>
-    <Button name='themeLight' btnClass={btnClass}/>
-    <Button name='themeDark'  btnClass={btnClass}/>
-    <Button name='viewSettings'   btnClass={btnClass}/>
-    <Button name='viewPath'   btnClass={btnClass}/>
-    <Button name='getOS'      btnClass={btnClass} onClick={() => getOS()}/>
-    <Button name='viewHome'   btnClass={btnClass}/>
-    <Button name='plane'      btnClass={btnClass} onClick={() => usls()}/>
+    <Button name='viewHome'     btnClass={btnClass}/>
+    <Button name='themeLight'   btnClass={btnClass}/>
+    <Button name='themeDark'    btnClass={btnClass}/>
+    <Button name='viewSettings' btnClass={btnClass}/>
+    <Button name='showMenu'     btnClass={btnClass}  popovertarget="popover-menu" onClick={() => null }/>
+    <ul class="dropdown menu w-52 rounded-box bg-base-100 shadow-sm" popover id="popover-menu" bind:this={popoverMenu}>
+      <li><Button name='viewPath'   btnClass={btnClass} labelRight onClick={() => handleSelect(viewPath)}>View Paths</Button></li>
+      <li><Button name='getOS'      btnClass={btnClass} labelRight onClick={() => handleSelect(getOS)}>OS Values</Button></li>
+      <li><Button name='plane'      btnClass={btnClass} labelRight onClick={() => handleSelect(usls)}>Model Shape</Button></li>
+      <li><Button name='openFolder' btnClass={btnClass} labelRight onClick={() => handleSelect(openFolder)}>Open Model</Button></li>
+    </ul>
   </div>
 </div>
 
